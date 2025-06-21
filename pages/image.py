e730d25056f587000fea7215f81cdfd833a0510e
Okgil <70752777+Okgil@users.noreply.github.com>
2025-06-21 13:09:58 +0900

    Create image.py

---
+++ pages/image.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.image('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExN3F5MHpwNzQwYWg2NGgxNWkxMjJzbG56bnNxZWZzODFyOHg0ZWZuNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pZDlzGjUwSguRH72Aa/giphy.gif')
